0bb9008490e28b1c0fb3266416d89c3217f84e31
Dashsoap <360062589@qq.com>
2019-04-11 19:11:51 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import App from './App.vue';
 import router from './router/router';
 import store from './store/store';
 import './registerServiceWorker';
+import TopMenu from './components/TopMenu.vue';
 
 import 'ant-design-vue/dist/antd.css';
+
+Vue.component('TopMenu', TopMenu);
 
 Vue.config.productionTip = false;
 Vue.use(Antd);
