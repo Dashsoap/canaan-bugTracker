597e96cb12eacaf4ca07760a63b4ae595a917f0e
Dashsoap <360062589@qq.com>
2019-04-15 15:45:39 +0800

    分步

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>
+    <!-- <div>
       <tr class="aboutTitle">
         <td class="canaanimg">
           <img src="../assets/Canaan.png" @click="homepoint()">
@@ -10,17 +10,145 @@
           <div>一个超级无敌牛逼的 IC 公司</div>
         </td>
       </tr>
-    </div>
+    </div>-->
     <!-- ----------------------------------------------- -->
     <!-- <div class="formContainer">
       <div class="myForm"></div>
       <div class="myDiv"></div>
     </div>-->
     <div>
-      <a-steps :current="current">
-        <a-step v-for="item in steps" :key="item.title" :title="item.title"/>
-      </a-steps>
-      <div class="steps-content"></div>
+      <a-row>
+        <a-col :span="6"></a-col>
+        <a-col :span="1"></a-col>
+        <a-col :span="11">
+          <h3>新问题向导</h3>
+        </a-col>
+        <a-col :span="6"></a-col>
+      </a-row>
+    </div>
+    <div>
+      <a-row>
+        <a-col :span="6"></a-col>
+        <a-col :span="1" :order="2">
+          <div class="content">
+            <a-steps direction="vertical" :current="current">
+              <a-step v-for="item in steps" :key="item.title" :title="item.title"/>
+            </a-steps>
+          </div>
+        </a-col>
+        <a-col :span="11" :order="2">
+          <div class="steps-content" v-if="current===0">
+            <div class="wizbody">
+              <h4>欢迎</h4>
+              <p>我们希望您输入最佳的问题报告，以便项目团队成员可以有效地采取行动。以下步骤将帮助您将问题发送给正确的人。</p>
+            </div>
+            <div class="wizbody">
+              <h4>确认版本号</h4>
+              <p>请确认下列信息是否正确</p>
+              <a-row style="height:32px">
+                <a-col :span="3" class="make-vertical">
+                  <div>SDK 版本号:</div>
+                </a-col>
+                <a-col :span="2">
+                  <a-select defaultValue="V0.5.3" style="width: 120px" @change="handleChange">
+                    <a-select-option value="none">无</a-select-option>
+                    <a-select-option value="0.5.3">V0.5.3</a-select-option>
+                    <a-select-option value="0.7.0">V0.7.0</a-select-option>
+                  </a-select>
+                </a-col>
+              </a-row>
+              <a-row style="height:32px">
+                <a-col :span="3" class="make-vertical">
+                  <div>HDK 版本号:</div>
+                </a-col>
+                <a-col :span="2">
+                  <a-select defaultValue="无" style="width: 120px" @change="handleChange">
+                    <a-select-option value="none">无</a-select-option>
+                    <a-select-option value="0.1.0">V0.1.0</a-select-option>
+                    <a-select-option value="0.2.0">V0.2.0</a-select-option>
+                  </a-select>
+                </a-col>
+              </a-row>
+              <a-row style="height:32px">
+                <a-col :span="3" class="make-vertical">
+                  <div>ToolChain:</div>
+                </a-col>
+                <a-col :span="2">
+                  <a-select
+                    defaultValue="Kendryte OpenOCD for Ubuntu x86_64(V 0.1.3)"
+                    style="width: 120px"
+                    @change="handleChange"
+                  >
+                    <a-select-option value="none">无</a-select-option>
+                    <a-select-option value="0.5.3">Kendryte OpenOCD for win32(V0.1.3)</a-select-option>
+                    <a-select-option value="0.7.0">Kendryte OpenOCD for Ubuntu x86_64(V 0.1.3)</a-select-option>
+                  </a-select>
+                </a-col>
+              </a-row>
+            </div>
+            <div class="wizbody">
+              <h4>帮助避免重复的问题报告</h4>
+              <a-checkbox @change="onChange">我搜索了 已经报告此问题的现有问题。</a-checkbox>
+            </div>
+          </div>
+
+          <div class="steps-content" v-if="current===1">
+            <div class="wizbody">
+              <h4>欢迎1</h4>
+              <p>我们希望您输入最佳的问题报告，以便项目团队成员可以有效地采取行动。以下步骤将帮助您将问题发送给正确的人。</p>
+            </div>
+            <div class="wizbody">
+              <h4>确认版本号</h4>
+              <p>请确认下列信息是否正确</p>
+              <a-row style="height:32px">
+                <a-col :span="3" class="make-vertical">
+                  <div>SDK 版本号:</div>
+                </a-col>
+                <a-col :span="2">
+                  <a-select defaultValue="V0.5.3" style="width: 120px" @change="handleChange">
+                    <a-select-option value="none">无</a-select-option>
+                    <a-select-option value="0.5.3">V0.5.3</a-select-option>
+                    <a-select-option value="0.7.0">V0.7.0</a-select-option>
+                  </a-select>
+                </a-col>
+              </a-row>
+              <a-row style="height:32px">
+                <a-col :span="3" class="make-vertical">
+                  <div>HDK 版本号:</div>
+                </a-col>
+                <a-col :span="2">
+                  <a-select defaultValue="无" style="width: 120px" @change="handleChange">
+                    <a-select-option value="none">无</a-select-option>
+                    <a-select-option value="0.1.0">V0.1.0</a-select-option>
+                    <a-select-option value="0.2.0">V0.2.0</a-select-option>
+                  </a-select>
+                </a-col>
+              </a-row>
+              <a-row style="height:32px">
+                <a-col :span="3" class="make-vertical">
+                  <div>ToolChain:</div>
+                </a-col>
+                <a-col :span="2">
+                  <a-select
+                    defaultValue="Kendryte OpenOCD for Ubuntu x86_64(V 0.1.3)"
+                    style="width: 120px"
+                    @change="handleChange"
+                  >
+                    <a-select-option value="none">无</a-select-option>
+                    <a-select-option value="0.5.3">Kendryte OpenOCD for win32(V0.1.3)</a-select-option>
+                    <a-select-option value="0.7.0">Kendryte OpenOCD for Ubuntu x86_64(V 0.1.3)</a-select-option>
+                  </a-select>
+                </a-col>
+              </a-row>
+            </div>
+            <div class="wizbody">
+              <h4>帮助避免重复的问题报告</h4>
+              <a-checkbox @change="onChange">我搜索了 已经报告此问题的现有问题。</a-checkbox>
+            </div>
+          </div>
+        </a-col>
+        <a-col :span="6"></a-col>
+      </a-row>
       <div class="steps-action">
         <a-button v-if="current < steps.length - 1" type="primary" @click="next">Next</a-button>
         <a-button
@@ -40,20 +168,7 @@ export default {
   data() {
     return {
       current: 0,
-      steps: [
-        {
-          title: "First",
-          content: "First-content"
-        },
-        {
-          title: "Second",
-          content: "Second-content"
-        },
-        {
-          title: "Last",
-          content: "Last-content"
-        }
-      ]
+      steps: [{}, {}, {}]
     };
   },
   methods: {
@@ -100,15 +215,44 @@ export default {
   border: 1px dashed #e9e9e9;
   border-radius: 6px;
   background-color: #fafafa;
-  min-height: 200px;
+  min-height: 400px;
   text-align: center;
-  padding-top: 80px;
 }
-
+.content {
+  display: flex;
+  min-height: 400px;
+  margin-top: 16px;
+  border-radius: 6px;
+  .ant-steps-item-tail {
+    height: 100px;
+  }
+}
 .steps-action {
   margin-top: 24px;
+  text-align: center;
 }
 .step {
   display: block;
+}
+
+h3 {
+  margin-top: 1rem;
+  font-size: medium;
+  margin-bottom: 0;
+  background: #e5ecf9;
+  border-top: 1px solid #3366cc;
+  padding: 0.5ex 0.5em 0.5ex 0.5em;
+}
+.wizbody {
+  background: #eee;
+  border: 1px solid #ddd;
+  padding: 1em;
+  margin-bottom: 1em;
+}
+.make-vertical {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
 </style>
