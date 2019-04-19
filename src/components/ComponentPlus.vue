5eda760550e709ed0cc7720e56465410a56313ae
Dashsoap <360062589@qq.com>
2019-04-19 15:23:32 +0800

    demo 的动态渲染

---
+++ src/components/ComponentPlus.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="(item,i) in myjson" :key="i">
+            {{item}}      
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"ComponentPlus",
+   data() {
+       return {
+           key: value
+       }
+   },
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,45 @@
 <template>
   <div class="home">
-
+    {{myjson}}
+    <div v-for="(item,i) in myjson" :key="i">
+      <label v-if="item.type === 'input'">
+        {{item.label}}
+        <input v-model="item.value">
+      </label>
+      <div v-else-if="item.type === 'radio'">
+        <span>{{item.label}}</span>
+        <label v-for="(option,j) in item.options" :key="`option-${i}-${j}`">
+          {{option.label}}
+          <input type="radio" :value="option.value" v-model="item.value">
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
-  name: 'home',
+  name: "home",
+  data() {
+    return {
+      myjson: [
+        { type: "input", label: "姓名", value: "" },
+        { type: "input", label: "年龄", value: "" },
+        {
+          type: "radio",
+          label: "单选",
+          options: [
+            {
+              label: "选项1",
+              value: 1
+            },
+            { label: "选项2", value: 2 }
+          ],
+          value: ""
+        }
+      ]
+    };
+  }
 };
 </script>
