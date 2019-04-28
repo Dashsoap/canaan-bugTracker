fb31c261eb3c5a3b0ab00aa011e78593e9b98cce
Dashsoap <360062589@qq.com>
2019-04-28 11:29:25 +0800

    可以用了

--- src/components/FContainer/FContainer.vue
+++ src/components/FContainer/FContainer.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-      <component v-for="(item,i) in src" :is="item.type" :key="i" :src="item"></component>
+    <div v-for="(item,i) in src" key="i" class="wizbody">
+      <h4>{{item.title}}</h4>
+      <p>{{item.description}}</p>
+      <component v-for="(content,j) in item.fitems" :is="content.type" :src="content" :key="`i-j`"></component>
+      {{item.fitems}}
+    </div>
   </div>
 </template>
 
@@ -12,9 +17,23 @@ export default {
       type: Array,
       required: true
     }
+  },
+  mounted() {
+    this.logs();
+  },
+  methods: {
+    logs() {
+      console.log(this.src);
+    }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.wizbody {
+  background: #eee;
+  border: 1px solid #ddd;
+  padding: 1em;
+  margin-bottom: 1em;
+}
 </style>
--- src/views/Home/data/source.js
+++ src/views/Home/data/source.js
@@ -1,37 +1,57 @@
 export default
 [
-  { type: 'f-single-line', label: '姓名', value: '' },
-  { type: 'f-single-line', label: '年龄', value: '' },
   {
-    type: 'f-radio',
-    label: '单选',
-    value: '',
-    options: [
-      {
-        label: '选项1',
-        value: 1,
-      },
-      { label: '选项2', value: 2 },
-      { label: '选项3', value: 3 },
+    title: '确认个人信息',
+    description: '请确保以下信息填写正确',
+    fitems: [
+      { type: 'f-single-line', label: '姓名', value: '' },
+      { type: 'f-single-line', label: '年龄', value: '' },
+    ],
+  }, {
+    title: '确认个人信息',
+    description: '请确保以下信息填写正确',
+    fitems: [
+      { type: 'f-single-line', label: '姓名', value: '' },
+      { type: 'f-single-line', label: '年龄', value: '' },
+    ],
+  }, {
+    title: '确认个人信息',
+    description: '请确保以下信息填写正确',
+    fitems: [
+      { type: 'f-single-line', label: '姓名', value: '' },
+      { type: 'f-single-line', label: '年龄', value: '' },
     ],
   },
-  {
-    type: 'f-select',
-    label: '产品',
-    value: '',
-    options: [
-      {
-        label: '选项1',
-        value: 1,
-      },
-      { label: '选项2', value: 2 },
-      { label: '选项3', value: 3 },
-    ],
-    tip: '1',
-  },
-  {
-    type: 'f-textarea',
-    label: '问题',
-    value: '',
-  },
+  // {
+  //   type: 'f-radio',
+  //   label: '单选',
+  //   value: '',
+  //   options: [
+  //     {
+  //       label: '选项1',
+  //       value: 1,
+  //     },
+  //     { label: '选项2', value: 2 },
+  //     { label: '选项3', value: 3 },
+  //   ],
+  // },
+  // {
+  //   type: 'f-select',
+  //   label: '产品',
+  //   value: '',
+  //   options: [
+  //     {
+  //       label: '选项1',
+  //       value: 1,
+  //     },
+  //     { label: '选项2', value: 2 },
+  //     { label: '选项3', value: 3 },
+  //   ],
+  //   tip: '1',
+  // },
+  // {
+  //   type: 'f-textarea',
+  //   label: '问题',
+  //   value: '',
+  // },
 ];
